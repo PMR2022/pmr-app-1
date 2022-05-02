96e942c085657c1da0d64584776929257f5b6c37
Thomas Bourdeaud'huy <thomas.bourdeaud_huy@centralelille.fr>
2022-05-02 16:04:31 +0200

    traces d'exécution

--- app/src/main/java/com/example/pmrapp1/MainActivity.java
+++ app/src/main/java/com/example/pmrapp1/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.pmrapp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CAT = "PMR2022";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(CAT,"onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(CAT,"onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(CAT,"onRestart");
     }
 }
